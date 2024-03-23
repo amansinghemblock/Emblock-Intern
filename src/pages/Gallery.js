596b7b590144b963ca3500cfd4808239d2d5d2de
amansinghemblock <amansingh.emblock@gmail.com>
2024-03-23 20:20:56 +0530

    initial files

---
+++ src/pages/Gallery.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Gallery = () => {
+  return (
+    <div>
+      <h1>Gallery Page</h1>
+      <p>View our gallery images.</p>
+    </div>
+  );
+};
+
+export default Gallery;
